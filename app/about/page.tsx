c76f08ac298e9a139eb06b16b1beee05bec54bcc
c-catanese <111301202+c-catanese@users.noreply.github.com>
2025-04-23 17:16:49 -0400

    about us page set up and image added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,20 @@
+"use client"
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+
 export default function About() {
 
   return (
-    <div className="bg-main w-screen h-screen">
-
+    <div className={styles.aboutContainer}>
+      <Image
+        width={320}
+        height={320}
+        className={styles.headshot}
+        src={'/headshot.jpeg'}
+        alt="picture"
+      />
+      
     </div>
   );
 }
